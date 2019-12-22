f9d99ba355a1a0b094b40fd008fb37a1a92d9779
huang <huangxa09@gmail.com>
2019-12-22 17:27:14 +0800

    新建应用，首首页样式修改

---
+++ my_platform/urls.py
@@ -0,0 +1,26 @@
+"""my_platform URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from user_manage import views
+
+urlpatterns = [
+    # 当包括其它 URL 模式时你应该总是使用 include() ， admin.site.urls 是唯一例外。
+    # path 参数：1.router 2.view 调用的视图
+    path('user_manage', include('user_manage.urls')),
+    # path('user_manage', views.index),
+    path('admin/', admin.site.urls),
+]
